13c476dcec2f7ba58adc2a530252f97366fd5018
AlineNunes2023 <alinenunespp@gmail.com>
2023-05-30 20:40:50 -0300

    tentativa2

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import data from './data/movies/movies.json' assert {type: "json"};
 const filmes = data
 console.log(filmes)
 
-//teste
-
 /*const exibeInformacoes = movies.map( movies => `${movies.nome}` + ' year: ' + `${movies.year}`);
 
 console.log(exibeInformacoes)*/
