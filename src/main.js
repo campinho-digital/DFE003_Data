f527b922bb815c4b5bd93387081e44e628a5ace0
LucianaChamma <107868796+LucianaChamma@users.noreply.github.com>
2023-06-01 21:46:47 -0300

    json

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import data from './data/movies/movies.json' assert {type: "json"};
 const filmes = data
 console.log(filmes)
 
+
+const movies =document.getElementById("cinema")
+
+data.forEach(function(item){
+  movies.innerHTML +=   
+  `
+    <div class="cards">
+      <h1>${item.Title}</h1>
+      <p>${item.Year}</p>
+      <p>${item.Director}</p>
+      <addr>${item.Images}</addr>
+    </div>
+  `
+})
+
+
+
+
+
+
 /*const exibeInformacoes = movies.map( movies => `${movies.nome}` + ' year: ' + `${movies.year}`);
 
 console.log(exibeInformacoes)*/                           
