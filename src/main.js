d535ea4df1395b3f54186b8fbdf5dc7878c14f6d
LucianaChamma <107868796+LucianaChamma@users.noreply.github.com>
2023-06-07 19:28:49 -0300

    card

--- src/main.js
+++ src/main.js
@@ -3,21 +3,20 @@ import dataMovies from './data/movies/movies.json' assert {type: "json"};
 const dataListMovies = dataMovies;
 console.log(dataListMovies);
 
-let htmlDivToMovie = document.getElementById("card-container")
-let htmlMovie
+let htmlDivToMovie = document.getElementById("card-container");
+let htmlMovie;
 
-function displayCards(data) {
-htmlMovie = dataMovies.map(({Title,Poster}) => {
+function displayCards() {
+htmlMovie = dataListMovies.map(({Title,Poster}) => {
 return (htmlMovie =`<div class="card" id="card">
 <div class="gridContainerUp" id="gridContainerUp">
 
 <div class="title"> ${Title}</div>
 
 <img src="${Poster}"/>
-</div>
+
 <div class ="backgroundImg" id="backgroundImg">
 
-</div>
 </div>
 </div>
 </div>
@@ -25,6 +24,7 @@ return (htmlMovie =`<div class="card" id="card">
 `);
 });
 htmlDivToMovie.innerHTML = htmlMovie.join("");
+
 }
+
 displayCards();
-                      
