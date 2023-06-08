fbddb26d756c161c8bce0179d682e871a9602344
Daniel Henrique <acazbenassi@gmail.com>
2023-06-08 15:25:48 -0300

    inciando o card

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import listaBeatles from "./data/beatles_songs/beatles_songs.json" assert {type: json}
+const musicas = listaBeatles;
+const card = document.getElementById("apareceCard");
+
+
+
+musicas.forEach(elemento => {
+    card.innerHTML += `
+    <p> Nome do album: ${elemento.name}</p>
+    
+    
+    
+    `
+
+
+})
