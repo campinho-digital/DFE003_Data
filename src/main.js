b720e9b7ac839850f3296cfb39f7cc402d127f90
Matheus Gomes <matheusoutros@hotmail.com>
2023-06-09 14:27:31 -0300

    adicionando um carousel no projeto

--- src/main.js
+++ src/main.js
@@ -5,3 +5,14 @@ function exibeJson() {
 }
 
 exibeJson();
+
+
+document.querySelector(".container__carousel__div")
+    .addEventListener("wheel", e => {
+        // console.log(e)
+        if (e.deltaY > 0) {
+            e.target.scrollBy(300, 0)
+        } else {
+            e.target.scrollBy(-300, 0)
+        }
+    })
