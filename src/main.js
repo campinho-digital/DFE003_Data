08562ffc918da3b73527ecb7b8e43282716367da
Matheus Gomes <matheusoutros@hotmail.com>
2023-06-11 22:02:44 -0300

    puxei o commit da branch carousel, estava no lugar errado

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import arquivoJson from "./beatles_songs.json" assert { type: "json" };
 
 function exibeJson() {
@@ -5,3 +6,14 @@ function exibeJson() {
 }
 
 exibeJson();
+=======
+document.querySelector(".container__carousel__div")
+    .addEventListener("wheel", e => {
+        // console.log(e)
+        if (e.deltaY > 0) {
+            e.target.scrollBy(300, 0)
+        } else {
+            e.target.scrollBy(-300, 0)
+        }
+    })
+>>>>>>> acc54bd (alterações prontas)
